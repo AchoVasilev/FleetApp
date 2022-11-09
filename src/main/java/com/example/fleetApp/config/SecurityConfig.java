8174e7091e602b927076307b30cf8b7cbdddcf7c
Angel Vasilev <angavasilev@gmail.com>
2022-11-09 17:36:26 +0200

    fix sidebar fragment

---
+++ src/main/java/com/example/fleetApp/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.fleetApp.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .anyRequest().permitAll();
+
+        return httpSecurity.build();
+    }
+}
